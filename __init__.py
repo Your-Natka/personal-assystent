29b5380cd61fb4eda0061768292074c24770c9a1
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-07-17 12:18:02 +0200

    file_read.me

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .commands import execute_command
 from .parser import parse_input
-from .storage import add_contact, add_full_contact, edit_email, edit_address, change_contact, show_phone, show_all
+from .storage import add_contact_interactive, edit_contact_interactive, edit_email, edit_address, change_contact, show_contact, show_phone, show_all
 from .error_handlers import input_error
 
-__all__ = ["execute_command", "parse_input", "add_contact", "add_full_contact", "edit_contact", "edit_email", "edit_address", "change_contact", "show_phone", "show_all", "input_error"]
+__all__ = ["execute_command", "parse_input", "add_contact_interactive", "edit_contact_interactive", "edit_email", "edit_address", "change_contact", "show_contact", "show_phone", "show_all", "input_error"]
